f4f26143426ebf93b499fcc23ff981c9f0412ff3
Romain Catajar <rcatajar@gmail.com>
2020-03-04 10:13:59 +0100

    Fix pk resources crash

--- authtools/__init__.py
+++ authtools/__init__.py
@@ -1,3 +1,4 @@
-import pkg_resources
+#import pkg_resources
 
-__version__ = pkg_resources.get_distribution('django-authtools').version
+#__version__ = pkg_resources.get_distribution('django-authtools').version
+__version__ = "1.8.0.dev"
